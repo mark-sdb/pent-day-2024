5e033fda306cccb9f5c6cd57b5c384b4f3ea31c1
Mark <mark.mooibroek@sdbgroep.nl>
2024-05-13 08:50:53 +0200

    add missing file

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:pent_day_2024/l10n/app_localizations.dart';
+
+class L10n {
+  static AppLocalizations of(BuildContext context) {
+    return AppLocalizations.of(context)!;
+  }
+}
