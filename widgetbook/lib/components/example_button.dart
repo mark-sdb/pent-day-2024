d4f30f1e44db584042fbc734429c5d1a238e99e4
Mark <mark.mooibroek@sdbgroep.nl>
2024-05-13 12:17:11 +0200

    Add localisation for example button

--- widgetbook/lib/components/example_button.dart
+++ widgetbook/lib/components/example_button.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:pent_day_2024/components/example_button.dart';
+import 'package:pent_day_2024/l10n/l10n.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(name: 'Default', type: ExampleButton)
 Widget buildCoolButtonUseCase(BuildContext context) {
+
+  final l10nEnabled = context.knobs.boolean(label: 'Enable localization', initialValue: false);
+  final label = l10nEnabled ? L10n.of(context).exampleButtonText : context.knobs.string(label: 'Button text', initialValue: 'Click me!');
+
   return ExampleButton(
     onTap: () {},
-    labelText: context.knobs.string(label: 'Button text', initialValue: 'Click me!'),
+    labelText: label,
   );
 }
